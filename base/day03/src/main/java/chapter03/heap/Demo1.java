14e376a6773c5c6a3836c289e8a341f8ddc3d5a0
SpringSunYY <3217778348@qq.com>
2024-12-01 23:31:09 +0800

    运行时数据区 程序计数器（不会栈内存溢出）、Java虚拟机栈、本地方法栈、堆内存

--- base/day03/src/main/java/chapter03/heap/Demo1.java
+++ base/day03/src/main/java/chapter03/heap/Demo1.java
@@ -1,6 +1,8 @@
 package chapter03.heap;
 
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryUsage;
 import java.util.ArrayList;
 
 /**
@@ -9,15 +11,15 @@ import java.util.ArrayList;
 public class Demo1 {
     public static void main(String[] args) throws InterruptedException, IOException {
         //JMX打印堆内存
-//        MemoryUsage heapMemoryUsage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
-//        System.out.println(heapMemoryUsage.getMax());
+        MemoryUsage heapMemoryUsage = ManagementFactory.getMemoryMXBean().getHeapMemoryUsage();
+        System.out.println(heapMemoryUsage.getMax());
 
 
         ArrayList<Object> objects = new ArrayList<Object>();
-        System.in.read();
         while (true){
+//            System.in.read();
             objects.add(new byte[1024 * 1024 * 100]);
-            Thread.sleep(1000);
+            Thread.sleep(1500);
         }
 
 
